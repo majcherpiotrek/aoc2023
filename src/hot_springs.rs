accd1311ca6e8ce3fc2bd45518f2178483bc5795
Piotr Majcher <majcherpiotr.dev@gmail.com>
2023-12-14 21:45:32 +0000

    day 12 part 2

--- src/hot_springs.rs
+++ src/hot_springs.rs
@@ -1,300 +1,136 @@
-struct Row(Vec<SpringState>, Vec<usize>);
+use std::collections::HashMap;
 
-impl Row {
-    pub fn parse(line: &str, unfold_records: bool) -> Row {
-        let mut line_split = line.split_whitespace();
-        let springs = line_split
-            .next()
-            .map(|s| {
-                let record = if unfold_records {
-                    (0..5)
-                        .map(|_| s.to_string())
-                        .collect::<Vec<String>>()
-                        .join("?")
-                } else {
-                    s.to_string()
-                };
-                record
-                    .chars()
-                    .map(|c| SpringState::from_char(&c))
-                    .collect::<Vec<SpringState>>()
-            })
-            .expect("Invalid input");
-
-        let groups = line_split
-            .next()
-            .map(|s| {
-                let groups = if unfold_records {
-                    (0..5)
-                        .map(|_| s.to_string())
-                        .collect::<Vec<String>>()
-                        .join(",")
-                } else {
-                    s.to_string()
-                };
-                groups
-                    .split(",")
-                    .map(|n| n.parse::<usize>().expect("Invalid input"))
-                    .collect::<Vec<usize>>()
-            })
-            .expect("Invalid input");
-
-        Row(springs, groups)
-    }
-
-    pub fn to_tuple(&self) -> (&Vec<SpringState>, &Vec<usize>) {
-        (&self.0, &self.1)
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum SpringState {
-    Operational,
-    Broken,
-    Unknown,
-}
-
-impl SpringState {
-    pub fn from_char(c: &char) -> SpringState {
-        match c {
-            '.' => SpringState::Operational,
-            '#' => SpringState::Broken,
-            '?' => SpringState::Unknown,
-            _ => panic!("Invalid spring state"),
-        }
-    }
-}
-
-impl std::fmt::Display for SpringState {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(match self {
-            SpringState::Broken => "#",
-            SpringState::Unknown => "?",
-            SpringState::Operational => ".",
-        })
-    }
-}
-
-pub fn sum_possible_arrangements(file: &str, unfold_records: bool) -> usize {
-    let springs_records = parse_input(file, unfold_records);
-    let mut sum: usize = 0;
-    for record in springs_records.iter() {
-        //println!(
-        //    "\n\nRecord {i}, broken blocks: {}\n\n",
-        //    record
-        //        .1
-        //        .iter()
-        //        .map(|n| n.to_string())
-        //        .collect::<Vec<String>>()
-        //        .join(",")
-        //);
-        //println!("Unfiltered");
-        //println!(
-        //    "{}",
-        //    record
-        //        .0
-        //        .iter()
-        //        .map(|s| s.to_string())
-        //        .collect::<Vec<String>>()
-        //        .join("")
-        //);
-
-        let arrangements = find_possibilities_per_block(record);
-
-        //for (block_num, arrangements_for_block) in arrangements.iter().enumerate() {
-        //   // println!(
-        //   //     "Block num: {block_num} - size {}",
-        //   //     record.1.get(block_num).expect("This value must be there")
-        //   // );
-        //    for arr in arrangements_for_block.iter() {
-        //        let (start, end) = arr;
-        //        let springs = &record.0;
-        //        let head = &springs[0..*start]
-        //            .iter()
-        //            .map(|s| s.to_string())
-        //            .collect::<Vec<String>>()
-        //            .join("");
-        //        let placement = (0..(end - start))
-        //            .map(|_| "X")
-        //            .collect::<Vec<&str>>()
-        //            .join("");
-        //        let tail = &springs[*end..]
-        //            .iter()
-        //            .map(|s| s.to_string())
-        //            .collect::<Vec<String>>()
-        //            .join("");
-
-        //        println!("{head}{placement}{tail}");
-        //    }
-        //    println!("\n");
-        //}
-        println!("Arrangements {}", arrangements.len());
-        let possibilities = find_all(&record.0, &Vec::new(), &arrangements);
-        println!("Possibilities {}", possibilities);
-        //for (n, p) in possibilities.iter().enumerate() {
-        //    for arr in p.iter() {
-        //        let (start, end) = arr;
-        //        let springs = &record.0;
-        //        let head = &springs[0..*start]
-        //            .iter()
-        //            .map(|s| s.to_string())
-        //            .collect::<Vec<String>>()
-        //            .join("");
-        //        let placement = (0..(end - start))
-        //            .map(|_| "X")
-        //            .collect::<Vec<&str>>()
-        //            .join("");
-        //        let tail = &springs[*end..]
-        //            .iter()
-        //            .map(|s| s.to_string())
-        //            .collect::<Vec<String>>()
-        //            .join("");
-
-        //        println!("{}: {head}{placement}{tail}", n + 1);
-        //    }
-        //}
-        sum += possibilities;
-    }
-
-    sum
-}
-
-fn find_possibilities_per_block(row: &Row) -> Vec<Vec<(usize, usize)>> {
-    let (springs, broken_blocks_sizes) = row.to_tuple();
-
-    let possible_arrangements_per_block: Vec<Vec<(usize, usize)>> = broken_blocks_sizes
-        .iter()
-        .enumerate()
-        .map(|(i, size)| {
-            //println!("Block {i}, size {size}");
-            let mut possible_positions: Vec<(usize, usize)> = Vec::new();
-
-            let number_of_preceeding_broken_blocks = &broken_blocks_sizes[0..i]
-                .iter()
-                .fold(0, |acc, elem| acc + elem);
-            let number_of_succeeding_broken_blocks = &broken_blocks_sizes[(i + 1)..]
-                .iter()
-                .fold(0, |acc, elem| acc + elem);
-
-            let mut lowest_possible_slice_start = i + number_of_preceeding_broken_blocks;
-
-            loop {
-                if lowest_possible_slice_start > springs.len() - size {
-                    panic!("This should never happen, impossible block");
-                }
-
-                let maybe_next_spring = springs.get(lowest_possible_slice_start + size);
-
-                let is_succeeded_by_broken_spring = maybe_next_spring
-                    .map(|s| *s == SpringState::Broken)
-                    .unwrap_or(false);
-
-                if lowest_possible_slice_start == 0 {
-                    if is_succeeded_by_broken_spring {
-                        lowest_possible_slice_start += 1;
-                        continue;
-                    } else {
-                        break;
-                    }
-                }
-
-                let actual_preceeding_broken_springs = &springs[0..lowest_possible_slice_start]
-                    .iter()
-                    .fold(0 as usize, |acc, elem| {
-                        if *elem == SpringState::Broken {
-                            acc + 1
-                        } else {
-                            acc
-                        }
-                    });
-
-                let is_preceeded_by_broken_spring = *actual_preceeding_broken_springs
-                    > *number_of_preceeding_broken_blocks
-                    || *springs
-                        .get(lowest_possible_slice_start - 1)
-                        .expect("This should always be within bounds")
-                        == SpringState::Broken;
-
-                if is_preceeded_by_broken_spring || is_succeeded_by_broken_spring {
-                    lowest_possible_slice_start += 1;
-                    continue;
-                }
-
-                break;
+pub fn parse(line: &str, unfold_records: bool) -> (String, Vec<usize>) {
+    let mut line_split = line.split_whitespace();
+    let springs = line_split
+        .next()
+        .map(|s| {
+            if unfold_records {
+                (0..5)
+                    .map(|_| s.to_string())
+                    .collect::<Vec<String>>()
+                    .join("?")
+            } else {
+                s.to_string()
             }
-
-            let highest_possible_end = springs.len() - number_of_succeeding_broken_blocks;
-
-            for start in lowest_possible_slice_start..=(highest_possible_end - size) {
-                let end: usize = start + size;
-                let slice = &springs[start..end];
-
-                let are_all_springs_broken_or_unknown = slice
-                    .iter()
-                    .all(|s| *s == SpringState::Unknown || *s == SpringState::Broken);
-
-                if are_all_springs_broken_or_unknown {
-                    possible_positions.push((start, end));
-                }
-            }
-
-            possible_positions
         })
-        .collect();
+        .expect("Invalid input");
 
-    possible_arrangements_per_block
-}
-
-fn find_all(
-    springs: &Vec<SpringState>,
-    head: &Vec<(usize, usize)>,
-    tail_possibilities: &Vec<Vec<(usize, usize)>>,
-) -> usize {
-    if tail_possibilities.is_empty() {
-        //let springs_slice = match head.last() {
-        //    Some(last) => &springs[0..last.1],
-        //    None => &springs[..]
-        //};
-        let is_valid_combination = springs.iter().enumerate().all(|(i, spring)| match spring {
-            SpringState::Broken => head.iter().any(|(start, end)| i >= *start && i < *end),
-            _ => true,
-        });
-        return if is_valid_combination {
-           1 
-        } else {
-          0 
-        };
-    }
-
-    match tail_possibilities.first() {
-        None => 1,
-        Some(next_level_possibilities) => {
-            let valid_possibilities = match head.last() {
-                None => next_level_possibilities
-                    .iter()
-                    .collect::<Vec<&(usize, usize)>>(),
-                Some(last_block) => next_level_possibilities
-                    .iter()
-                    .filter(|block| block.0 > last_block.1)
-                    .collect::<Vec<&(usize, usize)>>(),
+    let groups = line_split
+        .next()
+        .map(|s| {
+            let groups = if unfold_records {
+                (0..5)
+                    .map(|_| s.to_string())
+                    .collect::<Vec<String>>()
+                    .join(",")
+            } else {
+                s.to_string()
             };
+            groups
+                .split(",")
+                .map(|n| n.parse::<usize>().expect("Invalid input"))
+                .collect::<Vec<usize>>()
+        })
+        .expect("Invalid input");
 
-            let mut all_possibilities: usize = 0; 
-            for possibility in valid_possibilities.into_iter() {
-                let mut new_head = head.clone();
-                new_head.push(*possibility);
-                let following_levels = &tail_possibilities[1..].to_vec();
-                let mut following_possibilities = find_all(springs, &new_head, following_levels);
-                all_possibilities += following_possibilities
-            }
-            all_possibilities
-        }
-    }
+    (springs, groups)
 }
 
-fn parse_input(file: &str, unfold_records: bool) -> Vec<Row> {
+pub fn calculate_arrangements(file: &str, unfold_records: bool) -> usize {
+    let records = parse_input(file, unfold_records);
+    records.iter().fold(0, |acc, record| {
+        let (springs, groups) = record;
+        let mut cache: HashMap<String, usize> = HashMap::new();
+        let arrangements = solve(springs, groups, 0, &mut cache);
+        acc + arrangements
+    })
+}
+
+fn parse_input(file: &str, unfold_records: bool) -> Vec<(String, Vec<usize>)> {
     file.split("\n")
         .filter(|line| !line.is_empty())
-        .map(|line| Row::parse(line, unfold_records))
-        .collect::<Vec<Row>>()
+        .map(|line| parse(line, unfold_records))
+        .collect::<Vec<(String, Vec<usize>)>>()
+}
+
+fn get_cache_key(springs: &str, groups: &[usize], group_counter: usize) -> String {
+    format!("{}-{:?}-{}", springs, groups, group_counter)
+}
+
+fn solve(
+    springs: &str,
+    groups: &[usize],
+    group_counter: usize,
+    cache: &mut HashMap<String, usize>,
+) -> usize {
+    let cache_key = get_cache_key(springs, groups, group_counter);
+
+    if let Some(cached_result) = cache.get(&cache_key) {
+        return *cached_result;
+    }
+
+    if springs.is_empty() {
+        if groups.is_empty() {
+            cache.insert(cache_key, 1);
+            return 1;
+        } else if groups.len() == 1 && group_counter == groups[0] {
+            cache.insert(cache_key, 1);
+            return 1;
+        } else {
+            cache.insert(cache_key, 0);
+            return 0;
+        }
+    }
+
+    let current_spring = springs.chars().next().expect("This should never be empty");
+
+    if groups.is_empty() && springs.chars().all(|c| c == '.' || c == '?') {
+        cache.insert(cache_key, 1);
+        return 1;
+    } else if groups.is_empty() {
+        cache.insert(cache_key, 0);
+        return 0;
+    }
+
+
+    match current_spring {
+        '.' => {
+            let current_group = groups.first().expect("Groups cannot be empty here");
+            if *current_group == group_counter {
+                solve(&springs[1..], &groups[1..], 0, cache)
+            } else if group_counter == 0 {
+                solve(&springs[1..], groups, 0, cache)
+            } else {
+                cache.insert(cache_key, 0);
+                0
+            }
+        }
+        '#' => {
+            let updated_counter = group_counter + 1;
+            let current_group = groups.first().expect("Groups cannot be empty here");
+
+            if updated_counter > *current_group {
+                cache.insert(cache_key, 0);
+                0
+            } else {
+                solve(&springs[1..], groups, updated_counter, cache)
+            }
+        }
+        '?' => {
+            let with_operational_spring = ".".to_string() + &springs[1..].to_string();
+
+            let with_broken_spring = "#".to_string() + &springs[1..].to_string();
+            let sum_as_if_operational_spring =
+                solve(&with_operational_spring, groups, group_counter, cache);
+            let sum_as_if_broken_spring = solve(&with_broken_spring, groups, group_counter, cache);
+
+            let sum = sum_as_if_operational_spring + sum_as_if_broken_spring;
+
+            cache.insert(cache_key, sum);
+
+            sum
+        }
+        _ => panic!("Invalid spring symbol"),
+    }
 }
